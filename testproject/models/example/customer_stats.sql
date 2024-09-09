4f8eb1283338eb24f66976b9dbd6e2bf160fb243
Michiel De Muynck <michieldemuynck@gmail.com>
2024-09-09 15:48:04 +0000

    Add solutions for for-loops exercise

--- testproject/models/example/customer_stats.sql
+++ testproject/models/example/customer_stats.sql
@@ -1,6 +1,10 @@
 select
     c_custkey as custkey,
-    coalesce(sum(orders.o_totalprice), 0) as total_amount_spent
+    coalesce(sum(orders.o_totalprice), 0) as total_amount_spent,
+    {% for orderstatus in var("order_statuses") %}
+        count(case when orders.o_orderstatus = '{{ orderstatus }}' then 1 end) as num_orders_with_status_{{ orderstatus }}
+        {% if not loop.last %},{% endif %}
+    {% endfor %}
 from {{ ref("customer") }}
 left join {{ ref("orders") }}
     on customer.c_custkey = orders.o_custkey
